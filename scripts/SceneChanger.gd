7b5070b2eb36870364e0e4f3dea2d22d2c01521f
Mansour Quddus <itsnothisuser@gmail.com>
2023-04-13 04:05:07 -0400

    fonts

---
+++ scripts/SceneChanger.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+onready var sprite = $Sprite
+export var next_scene = ""
+
+func _ready():
+	sprite.hide()
+
+func _on_SceneChanger_body_entered(body):
+	if body.name == "Player":
+		get_tree().change_scene(next_scene)
+		
+	else:
+		return false
