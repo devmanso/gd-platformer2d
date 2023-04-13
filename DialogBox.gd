ab9fa71f7e287bc56c3be6b97cf4f50a9f015ca5
Mansour Quddus <itsnothisuser@gmail.com>
2023-04-13 05:00:24 -0400

    dialog box improvements

--- DialogBox.gd
+++ DialogBox.gd
@@ -1,6 +1,10 @@
 extends Control
 
+# adapted from https://www.youtube.com/watch?v=kkLqW8WhCgg
+
 onready var label = $RichTextLabel
+onready var tween = $Tween
+onready var texture_rect = $TextureRect
 
 export var dialog = [
 	"Hello world"
@@ -16,11 +20,25 @@ func _process(delta):
 	if Input.is_action_pressed("ui_accept"):
 		load_dialog()
 
+func animate_text():
+	tween.interpolate_property(
+		label, # Node selected
+		"percent_visible", # selected property of node
+		0, 1, # from, to
+		1, # time (in sec)
+		
+		# effects
+		Tween.TRANS_LINEAR,
+		Tween.EASE_IN_OUT
+	)
+	tween.start()
+
 # check if there is any dialog, if there is, then put the dialog data from that
 # index, and store it in the label's text, then iterate through all indexes
 # until done
 func load_dialog():
 	if dialog_index < dialog.size():
 		label.text = dialog[dialog_index]
-	else: queue_free()
+		animate_text()
+	else: queue_free(); dialog_finished = true
 	dialog_index += 1
