7bcb19472bb30092591fa7133def8e5744581c36
Mansour Quddus <itsnothisuser@gmail.com>
2023-07-07 01:40:45 -0400

    gravity system

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -12,10 +12,11 @@ onready var animator = $AnimationPlayer
 
 export var walkspeed = 300
 export var jump_power = -900
-export var gravity = 2500
 export var acceleration = 0.25
 export var friction = 0.1
+export var gravity = 2500
 
+var inversed_jump_power = -1 * jump_power
 var can_play_jump_anim
 var idle = true
 var up
@@ -25,9 +26,25 @@ var is_attacking
 var jumps_made = 0
 var velocity = Vector2.ZERO
 var motion = Vector2.ZERO
+var upsideDown = false
+
+func flip_gravity():
+	gravity = -2500
+	upsideDown = true
+	sprite.flip_v = true
+
+func release_gravity():
+	gravity = 2500
+	upsideDown = false
+	sprite.flip_v = false
+	jump_power = -900
 
 func get_input():
 	var dir = 0
+	
+	if Input.is_action_pressed("gravity"):
+		flip_gravity()
+	
 #	if Input.is_action_pressed("attack"):
 #		right = false
 #		left = false
@@ -68,6 +85,13 @@ func get_input():
 		
 
 func kb_input():
+	
+	if Input.is_action_pressed("gravity"):
+		flip_gravity()
+	
+	if Input.is_action_pressed("release"):
+		release_gravity()
+	
 	if Input.is_action_pressed("right"):
 		velocity.x = walkspeed
 		animator.play("walking")
@@ -79,11 +103,13 @@ func kb_input():
 	else:
 		animator.play("RESET")
 	
-	if !is_on_floor():
+	if !is_on_floor() and !is_on_ceiling():
 		animator.play("single_frame_jump")
 	
 	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = jump_power
+	elif Input.is_action_pressed("jump") and is_on_ceiling():
+		velocity.y = inversed_jump_power
 	
 
 func _physics_process(delta):
