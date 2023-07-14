17a916d6fa7ebfec167cd01ea63350d5b14c1121
Mansour Quddus <itsnothisuser@gmail.com>
2023-07-13 23:16:37 -0400

    small enhancements to MovingPlatform, can now move on X axis

--- scripts/MovingPlatform.gd
+++ scripts/MovingPlatform.gd
@@ -2,23 +2,37 @@ extends StaticBody2D
 
 export var yUnitsMoved = 360
 export var speed = 300
+export var isYaxis = true
 var initPosition: Vector2
 var endPosition: Vector2
 var isMoving = false
 
 func _ready():
 	initPosition = position
-	endPosition = Vector2(initPosition.x, initPosition.y + yUnitsMoved)
+	if isYaxis:
+		endPosition = Vector2(initPosition.x, initPosition.y + yUnitsMoved)
+	else:
+		endPosition = Vector2(initPosition.x + yUnitsMoved, initPosition.y)
 
 func _process(delta):
-	if isMoving:
-		position.y += speed * delta
-		if speed > 0 and position.y >= endPosition.y:
-			position.y = endPosition.y
-			isMoving = false
-		elif speed < 0 and position.y <= endPosition.y:
-			position.y = endPosition.y
-			isMoving = false
+	if isYaxis:
+		if isMoving:
+			position.y += speed * delta
+			if speed > 0 and position.y >= endPosition.y:
+				position.y = endPosition.y
+				isMoving = false
+			elif speed < 0 and position.y <= endPosition.y:
+				position.y = endPosition.y
+				isMoving = false
+	else:
+		if isMoving:
+			position.x += speed * delta
+			if speed > 0 and position.x >= endPosition.x:
+				position.x = endPosition.x
+				isMoving = false
+			elif speed < 0 and position.x <= endPosition.x:
+				position.x = endPosition.x
+				isMoving = false
 
 func _on_ObjectDetector_body_entered(body):
 	if "Player" in body.name:
