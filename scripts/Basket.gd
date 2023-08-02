e5def7d6d451fb5449d855518cdad7a68160dab5
Mansour Quddus <itsnothisuser@gmail.com>
2023-08-02 17:33:11 -0400

    Basket sprite update

--- scripts/Basket.gd
+++ scripts/Basket.gd
@@ -1,12 +1,19 @@
 extends StaticBody2D
 
+onready var redSprite = $RedSprite
+onready var blueSprite = $BlueSprite
+
 var pinkTiles
 
 func handle_goal():
 	pinkTiles.hide()
 	pinkTiles.disable_collision()
+	blueSprite.show()
+	redSprite.hide()
 
 func _ready():
+	blueSprite.hide()
+	redSprite.show()
 	pinkTiles = get_parent().find_node("PinkCaveTMap")
 
 func _on_Goal_body_entered(body):
