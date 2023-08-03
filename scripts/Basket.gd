ad865be645e25af6d3ff7edef57dcb21c1c580a5
Mansour Quddus <itsnothisuser@gmail.com>
2023-08-03 02:07:24 -0400

    Basket fix

--- scripts/Basket.gd
+++ scripts/Basket.gd
@@ -4,6 +4,8 @@ onready var redSprite = $RedSprite
 onready var blueSprite = $BlueSprite
 
 var pinkTiles
+var ballLeft : bool
+var playerLeft : bool
 
 func handle_goal():
 	pinkTiles.hide()
@@ -11,11 +13,32 @@ func handle_goal():
 	blueSprite.show()
 	redSprite.hide()
 
+func reset():
+	pinkTiles.show()
+	pinkTiles.enable_collision()
+	blueSprite.hide()
+	redSprite.show()
+
 func _ready():
 	blueSprite.hide()
 	redSprite.show()
 	pinkTiles = get_parent().find_node("PinkCaveTMap")
 
 func _on_Goal_body_entered(body):
-	if "Ball" in body.name:
+	if "Player" in body.name:
 		handle_goal()
+		playerLeft = false
+	elif "Ball" in body.name:
+		handle_goal()
+		ballLeft = false
+
+func _on_Goal_body_exited(body):
+	if "Player" in body.name:
+		playerLeft = true
+	elif "Ball" in body.name:
+		ballLeft = true
+	
+
+func _process(delta):
+	if ballLeft and playerLeft:
+		reset()
